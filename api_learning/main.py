486d1bf5469351656d8b84827329224d3cfc046a
Maharshi Patel <144848033+maharshiPatel9094@users.noreply.github.com>
2024-11-04 12:49:09 -0400

    commit
    learned about api parameters

--- api_learning/main.py
+++ api_learning/main.py
@@ -4,16 +4,45 @@
 import requests
 
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# print(response)
-# to raise a request error
-response.raise_for_status()
-data = response.json()
-latitude = data["iss_position"]["latitude"]
-longitude = data["iss_position"]["longitude"]
+# ----------API PARAMETERS 
+# sunrise and sunset 
+
+# if the api has parameters inside it they will describe it like what it should take 
+# parameters can be required or optional
+# optional parameters have some default value 
+# key nme must be same as the api doc say 
+
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+parameters = {
+    "lat" : MY_LAT,
+    "lng" : MY_LONG
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameters)
+response.raise_for_status() 
+data = response.json
 print(data)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+
+
+
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # print(response)
+# # to raise a request error
+# response.raise_for_status()
+# data = response.json()
+# latitude = data["iss_position"]["latitude"]
+# longitude = data["iss_position"]["longitude"]
+# print(data)
 
 # response object has lot of things inside this object
+
 
 
 
