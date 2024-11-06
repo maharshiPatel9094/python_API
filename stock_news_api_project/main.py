54330e7b367e67c03f0625d8a205b703e5b68c10
Maharshi Patel <144848033+maharshiPatel9094@users.noreply.github.com>
2024-11-06 06:42:28 -0400

    commit
    final uodate we created a sms system using twilio

--- stock_news_api_project/main.py
+++ stock_news_api_project/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 
 # load env var
@@ -8,6 +9,8 @@ load_dotenv()
 
 api_key = os.getenv("API_KEY")
 news_api_key = os.getenv("NEWS_API_KEY")
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -40,17 +43,22 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
 #Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-diffrence = abs(float(yesterday_close_price) - float(day_before_yesterday_closing_price))
-print(diffrence)
+diffrence = float(yesterday_close_price) - float(day_before_yesterday_closing_price)
+# print(diffrence)
+up_down = None
+if diffrence > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
 #Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 # if we use day before closing price it will give us the percentage diffrence.
-diffrence_percentage = (diffrence/float(yesterday_close_price)) *  100
+diffrence_percentage = round((diffrence/float(yesterday_close_price)) *  100)
 print(diffrence_percentage)
 
 #If percentage is greater than 5 then print("Get News").
 #Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-if diffrence_percentage > 1:
+if abs(diffrence_percentage) > 5:
     news_params = {
         "apiKey" : news_api_key,
         "qInTitle" : COMPANY_NAME,
@@ -62,27 +70,23 @@ if diffrence_percentage > 1:
     # print(data)
 #Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     first_three_articles = data[:3]
-    print(first_three_articles)
+    # print(first_three_articles)
 
 
+#to send a separate message with each article's title and description to your phone number. 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+#Create a new list of the first 3 article's headline and description using list comprehension.
+article_list = [f"{STOCK_NAME}: {up_down}{diffrence_percentage}% Headline: {article['title']}. \nBrief: {article['description']}" for article in first_three_articles]
+print(article_list)
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
+#Send each article as a separate message via Twilio. 
+client = Client(account_sid, auth_token)
 
 
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+for article in article_list:
+    message = client.messages.create(
+        body= article,
+        from_='+12183166216',
+        to='+14375571728'
+        )
 
